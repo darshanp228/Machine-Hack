87b065d1049badc1a82b8eb31090af18540bb5f9
darshanp228 <72122805+darshanp228@users.noreply.github.com>
2021-04-21 18:12:53 +0530

    Update CAT_model.py

--- Predicting House Prices In Bengaluru/Codes/CAT_model.py
+++ Predicting House Prices In Bengaluru/Codes/CAT_model.py
@@ -10,39 +10,30 @@ from catboost import Pool, CatBoostRegressor
 import pandas as pd
 import numpy as np
 
-
+#Get Trainging data
 dataset = pd.read_csv('Train.csv')
 X_train = dataset.iloc[:, :-1].values
 y_train = dataset.iloc[:, 8].values
-#X = dataset.iloc[:, :-1].values
-#y = dataset.iloc[:, 8].values
 
-
+#Get Testing data
 dataset = pd.read_csv('Test.csv')
 X_test = dataset.iloc[:, :-1].values
 
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+#Preprocessing Training data
 X_train = pd.DataFrame(X_train)
 X_train = PreProcessing(X_train)
 
+#Preprocessing Testing data
 X_test = pd.DataFrame(X_test)
 X_test = PreProcessing(X_test)
 
 features = ['Area_Type', 'Location']
 train_data = X_train
 train_label = y_train
-#eval_data = X_test
-#eval_label = y_test
 
 train_dataset = Pool(data=train_data,
                      label=train_label,
                      cat_features=features)
-
-#eval_dataset = Pool(data=eval_data,
-#                    label=eval_label,
-#                    cat_features=features)
-
 
 # Initialize CatBoostClassifier
 model = CatBoostRegressor(
@@ -53,13 +44,14 @@ model = CatBoostRegressor(
                         verbose=False)
    
 # Fit model with `use_best_model=True`
-
 model.fit(train_dataset,
           use_best_model=True,
           plot=True)
 
+#Predict outputs for Test data
 y_pred = model.predict(X_test)
 
+#Calculate results
 CATresults = 1 - np.sqrt(np.square(np.log10(y_pred +1) - np.log10(y_test +1)).mean())
 print(CATresults)
 
